e69aab71629da4de8390d7e72b93dbdc401dc76f
Kazuki Noto <kazuki.noto.0206@gmail.com>
2022-11-07 01:53:38 +0000

    項目の追加とロードの確認

--- toml_try.py
+++ toml_try.py
@@ -12,3 +12,26 @@ print(data)
 print(data['tittle'])
 
 print(data["setting"]["app_name"])
+
+"""
+配列の読み込み
+[2018, 2019, 2020]
+"""
+year_list = data["setting"]["year_list"]
+print(year_list)
+ 
+
+"""
+マイナス値の読み込み
+-10
+"""
+print(data["setting"]["minus_value"])
+
+
+"""
+ブーリアン値の読み込み
+True
+False
+"""
+print(data["setting"]["is_true"])
+print(data["setting"]["is_false"])
